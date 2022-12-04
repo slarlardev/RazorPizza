8620ec68c384142a28778239d7cf3412c4af8b07
slarlardeveloper <slarlardeveloper@outlook.com>
2022-12-04 09:19:26 +0300

    Pizza page rename

--- RazorPizza/Pages/PizzaSale.cshtml.cs
+++ RazorPizza/Pages/PizzaSale.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizza.Pages
 {
-    public class PizzaModel : PageModel
+    public class PizzaSaleModel : PageModel
     {
         public void OnGet()
         {
