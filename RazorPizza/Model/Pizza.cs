8cee551612cc166e81965f5395e1e64d7c3b77fb
slarlardeveloper <slarlardeveloper@outlook.com>
2022-12-04 09:21:27 +0300

    Pizza model added

---
+++ RazorPizza/Model/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorPizza.Model
+{
+    public class Pizza
+    {
+        public string ImageTitle { get; set; }
+        public string Name { get; set; }
+        public float BasePrice { get; set; }
+        public bool Saouce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Peperoni { get; set; }
+    }
+}
