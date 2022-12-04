a02065f346d83dd3ca5cbabb784aa76cf09c6626
slarlardeveloper <slarlardeveloper@outlook.com>
2022-12-04 10:23:52 +0300

    Custom Pizza page model completed

--- RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -9,8 +9,20 @@ namespace RazorPizza.Pages.Forms
         [BindProperty]
         public Pizza Pizza { get; set; }
 
+        public float PizzaPrice { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult  OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+            PizzaPrice += Pizza.Saouce ? 1 : 0;
+            PizzaPrice += Pizza.Cheese ? 1 : 0;
+            PizzaPrice += Pizza.Peperoni ? 1 : 0;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.Name, PizzaPrice });
         }
     }
 }
