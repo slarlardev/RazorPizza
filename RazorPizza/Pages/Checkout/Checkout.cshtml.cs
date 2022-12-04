689f2314f1d292ee601c11bfbdac875f329adcd1
slarlardeveloper <slarlardeveloper@outlook.com>
2022-12-04 13:05:22 +0300

    DbContext Dependency Injection, Saving Orders completed

--- RazorPizza/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizza/Pages/Checkout/Checkout.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Data;
+using RazorPizza.Model;
 
 namespace RazorPizza.Pages.Checkout
 {
@@ -10,6 +12,13 @@ namespace RazorPizza.Pages.Checkout
         public float PizzaPrice { get; set; }
         public string ImageTitle { get; set; }
 
+        private readonly AppDbContext _context;
+
+        public CheckoutModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
             if (string.IsNullOrWhiteSpace(Name))
@@ -17,6 +26,13 @@ namespace RazorPizza.Pages.Checkout
 
             if (string.IsNullOrWhiteSpace(ImageTitle))
                 ImageTitle = "Create";
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = Name;
+            pizzaOrder.Price = PizzaPrice;
+
+            _context.PizzaOrders.Add(pizzaOrder);
+            _context.SaveChanges();
         }
     }
 }
